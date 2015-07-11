a8bdbeb904e2b5c89375f64759d99b800b463e2d
Nguyễn Minh Hiếu <hieunguyenminh.93@gmail.com>
2015-07-11 23:35:44 +0700

    new debug

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -6,17 +7,16 @@
  * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
-
 return array(
     'router' => array(
         'routes' => array(
             'home' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
-                    'route'    => '/',
+                    'route' => '/',
                     'defaults' => array(
                         'controller' => 'Application\Controller\Index',
-                        'action'     => 'index',
+                        'action' => 'index',
                     ),
                 ),
             ),
@@ -25,24 +25,24 @@ return array(
             // module. Simply drop new controllers in, and you can access them
             // using the path /application/:controller/:action
             'application' => array(
-                'type'    => 'Literal',
+                'type' => 'Literal',
                 'options' => array(
-                    'route'    => '/application',
+                    'route' => '/application',
                     'defaults' => array(
                         '__NAMESPACE__' => 'Application\Controller',
-                        'controller'    => 'Index',
-                        'action'        => 'index',
+                        'controller' => 'Index',
+                        'action' => 'index',
                     ),
                 ),
                 'may_terminate' => true,
                 'child_routes' => array(
                     'default' => array(
-                        'type'    => 'Segment',
+                        'type' => 'Segment',
                         'options' => array(
-                            'route'    => '/[:controller[/:action]]',
+                            'route' => '/[:controller[/:action]]',
                             'constraints' => array(
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
                             ),
                             'defaults' => array(
                             ),
@@ -59,58 +59,86 @@ return array(
         ),
         'factories' => array(
             'translator' => 'Zend\Mvc\Service\TranslatorServiceFactory',
-        ),
-    ),
-    'translator' => array(
-        'locale' => 'en_US',
-        'translation_file_patterns' => array(
-            array(
-                'type'     => 'gettext',
-                'base_dir' => __DIR__ . '/../language',
-                'pattern'  => '%s.mo',
+            'Zend\Db\Adapter\Adapter' => function ($sm) {
+                $adapter = new BjyProfiler\Db\Adapter\ProfilingAdapter(array(
+                    'driver' => 'pdo',
+                    'dsn' => 'mysql:dbname=radius;host=127.0.0.1',
+                    'database' => 'radius',
+                    'username' => 'root',
+                    'password' => '1234',
+                    'hostname' => '127.0.0.1',
+                ));
+
+                if (php_sapi_name() == 'cli') {
+                    $logger = new Zend\Log\Logger();
+                    // write queries profiling info to stdout in CLI mode
+                    $writer = new Zend\Log\Writer\Stream('php://output');
+                    $logger->addWriter($writer, Zend\Log\Logger::DEBUG);
+                    $adapter->setProfiler(new BjyProfiler\Db\Profiler\LoggingProfiler($logger));
+                } else {
+                    $adapter->setProfiler(new BjyProfiler\Db\Profiler\Profiler());
+                }
+                if (isset($dbParams['options']) && is_array($dbParams['options'])) {
+                    $options = $dbParams['options'];
+                } else {
+                    $options = array();
+                }
+                $adapter->injectProfilingStatementPrototype($options);
+                return $adapter;
+            },
+                ),
             ),
-        ),
-    ),
-    'controllers' => array(
-        'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
-        ),
-    ),
-    'view_manager' => array(
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
-        'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ),
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-    ),
-	'doctrine' => array(
-        'driver' => array(
-            'application_entities' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => array(__DIR__ . '/../src/Application/Entity')
+            'translator' => array(
+                'locale' => 'vi_VN',
+                'translation_file_patterns' => array(
+                    array(
+                        'type' => 'phpArray',
+                        'base_dir' => __DIR__ . '/../language' . __NAMESPACE__,
+                        'pattern' => '%s.php',
+                    ),
+                ),
             ),
-            'orm_default' => array(
-                'drivers' => array(
-                    'Application\Entity' => 'application_entities'
+            'controllers' => array(
+                'invokables' => array(
+                    'Application\Controller\Index' => 'Application\Controller\IndexController'
+                ),
+            ),
+            'view_manager' => array(
+                'display_not_found_reason' => true,
+                'display_exceptions' => true,
+                'doctype' => 'HTML5',
+                'not_found_template' => 'error/404',
+                'exception_template' => 'error/index',
+                'template_map' => array(
+                    'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+                    'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+                    'error/404' => __DIR__ . '/../view/error/404.phtml',
+                    'error/index' => __DIR__ . '/../view/error/index.phtml',
+                ),
+                'template_path_stack' => array(
+                    __DIR__ . '/../view',
+                ),
+            ),
+            'doctrine' => array(
+                'driver' => array(
+                    'application_entities' => array(
+                        'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                        'cache' => 'array',
+                        'paths' => array(__DIR__ . '/../src/Application/Entity')
+                    ),
+                    'orm_default' => array(
+                        'drivers' => array(
+                            'Application\Entity' => 'application_entities'
+                        )
+                    )
                 )
-            )
-        )
-    ),
-    // Placeholder for console routes
-    'console' => array(
-        'router' => array(
-            'routes' => array(
             ),
-        ),
-    ),
-);
+            // Placeholder for console routes
+            'console' => array(
+                'router' => array(
+                    'routes' => array(
+                    ),
+                ),
+            ),
+        );
+        
